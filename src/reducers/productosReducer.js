ad71d97bbce0fcd4a5045b9f40de5ff015b2eece
Javi Fernandez <javierfernandezriv@gmail.com>
2021-03-01 12:37:34 +0100

    Boilerplate for Redux project

---
+++ src/reducers/productosReducer.js
@@ -0,0 +1,15 @@
+import { AGREGAR_PRODUCTO, AGREGAR_PRODUCTO_EXITO, AGREGAR_PRODUCTO_ERROR } from './../types/index';
+
+//Cada reducer tiene su propio State
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
